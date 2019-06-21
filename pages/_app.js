8596e39b0e9fa5db7fe564983031f9cd17ecb096
CF12 <brianx0011@gmail.com>
2019-06-21 11:33:42 -0400

    Restructure styles

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,9 @@ import React from "react";
 import App, { Container } from "next/app";
 
 import { ThemeProvider } from "@rmwc/theme";
+import "normalize.css";
 
-import "../components/styles/normalize.css";
-import "../components/styles/global.scss";
+import "../styles/global.scss";
 import "@material/theme/dist/mdc.theme.css";
 
 class CustomApp extends App {
