52a0b47f9d145fa0394177003e06ec8f0b28620b
dumblole <move2234@gmail.com>
2019-06-24 16:09:19 -0400

    sync

--- components/AuthedPage.js
+++ components/AuthedPage.js
@@ -1,91 +1,103 @@
-import React from 'react'
+import React from "react";
 
-import Head from 'next/head'
-import Link from 'next/link'
-import {Drawer, DrawerHeader, DrawerTitle, DrawerContent} from '@rmwc/drawer'
-import {List, ListItem} from '@rmwc/list'
-import {Button} from '@rmwc/button'
-import {IconButton} from '@rmwc/icon-button'
-import {CircularProgress} from '@rmwc/circular-progress'
+import Head from "next/head";
+import Link from "next/link";
+import { Drawer, DrawerHeader, DrawerTitle, DrawerContent } from "@rmwc/drawer";
+import { List, ListItem } from "@rmwc/list";
+import { Button } from "@rmwc/button";
+import { IconButton } from "@rmwc/icon-button";
+import { CircularProgress } from "@rmwc/circular-progress";
 
-import '@material/drawer/dist/mdc.drawer.css'
-import '@material/list/dist/mdc.list.css'
-import '@material/button/dist/mdc.button.css';
-import '@material/icon-button/dist/mdc.icon-button.css';
-import '@rmwc/circular-progress/circular-progress.css';
+import "@material/drawer/dist/mdc.drawer.css";
+import "@material/list/dist/mdc.list.css";
+import "@material/button/dist/mdc.button.css";
+import "@material/icon-button/dist/mdc.icon-button.css";
+import "@rmwc/circular-progress/circular-progress.css";
 
-import css from "./styles/authedPage.scss"
+import css from "./styles/authedPage.scss";
 import MenuIcon from "./icons/material-icons/menu.svg";
 import AddIcon from "./icons/material-icons/add.svg";
 
-import firebase from 'firebase'
-import "firebase/firestore"
+import firebase from "firebase";
+import "firebase/firestore";
 
 function AuthedPage(props) {
   const [drawerOpen, setDrawerOpen] = React.useState(false);
-  const [db] = React.useState(firebase.firestore())
-  const [ctfs, setCTFs] = React.useState([])
+  const [db] = React.useState(firebase.firestore());
+  const [ctfs, setCTFs] = React.useState([]);
 
   React.useEffect(() => {
-    db.collection('ctfs').onSnapshot(snapshot => {
-      let res = []
+    db.collection("ctfs").onSnapshot(snapshot => {
+      let res = [];
 
       snapshot.forEach(doc => {
         res.push({
           id: doc.id,
           ...doc.data()
-        })
-      })
+        });
+      });
 
-      setCTFs(res)
-    })
-  }, [])
+      setCTFs(res);
+    });
+  }, []);
 
-  const ctfList = ctfs.map((c, i) => <Link as={`/ctf/${c.id}`} as={`/ctf/${c.id}`} href={`/ctf?id=${c.id}`}>
-    <a>
-      <ListItem key={i} className={css.ctfListItem}>
-        <p>{c.name}</p>
-        {c.dataFetched ? <></> : <CircularProgress/>}
-      </ListItem>
-    </a>
-  </Link>)
+  const ctfList = ctfs.map((c, i) => (
+    <Link as={`/ctf/${c.id}`} as={`/ctf/${c.id}`} href={`/ctf?id=${c.id}`} key={c.id}>
+      <a>
+        <ListItem key={i} className={css.ctfListItem}>
+          <p>{c.name}</p>
+          {c.dataFetched ? <></> : <CircularProgress />}
+        </ListItem>
+      </a>
+    </Link>
+  ));
 
-  return (<>
-    <Drawer modal open={drawerOpen} onClose={() => setDrawerOpen(false)} theme={["surface"]}>
-      <DrawerHeader>
-        <DrawerTitle>CTFSync</DrawerTitle>
-      </DrawerHeader>
-      <DrawerContent>
-        <Button label={"Add CTF"} icon={<AddIcon/>} onClick={() => {
-          db.collection("ctfs").add({
-            name: "HSCTF 2019",
-            url: "https://ctf.hsctf.com",
-            dataFetched: false
-          }).then(docRef => {
-            console.log(docRef.id)
-          }).catch(e => {
-            console.log(e)
-          })
-        }}/>
-        <List>
-          {ctfList}
-        </List>
-      </DrawerContent>
-    </Drawer>
+  return (
+    <>
+      <Drawer modal open={drawerOpen} onClose={() => setDrawerOpen(false)} theme={["surface"]}>
+        <DrawerHeader>
+          <DrawerTitle>CTFSync</DrawerTitle>
+        </DrawerHeader>
+        <DrawerContent>
+          <Button
+            label={"Add CTF"}
+            icon={<AddIcon />}
+            onClick={() => {
+              db.collection("ctfs")
+                .add({
+                  name: "HSCTF 2019",
+                  url: "https://ctf.hsctf.com",
+                  dataFetched: false
+                })
+                .then(docRef => {
+                  console.log(docRef.id);
+                })
+                .catch(e => {
+                  console.log(e);
+                });
+            }}
+          />
+          <List>{ctfList}</List>
+        </DrawerContent>
+      </Drawer>
 
-    <Head>
-      <title>yay a title</title>
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" key="viewport" />
-    </Head>
-    <header className={css.header}>
-      <IconButton className={css.menuIcon} icon={<MenuIcon/>} label="Open menu" onClick={() => setDrawerOpen(true)} />
-      <Link href="/">
-        <div className={css.siteName}>CTFSync</div>
-      </Link>
-    </header>
-    <div className={css.pageContent}>
-      {props.children}
-    </div>
-  </>)
+      <Head>
+        <title>yay a title</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" key="viewport" />
+      </Head>
+      <header className={css.header}>
+        <IconButton
+          className={css.menuIcon}
+          icon={<MenuIcon />}
+          label="Open menu"
+          onClick={() => setDrawerOpen(true)}
+        />
+        <Link href="/">
+          <div className={css.siteName}>CTFSync</div>
+        </Link>
+      </header>
+      <div className={css.pageContent}>{props.children}</div>
+    </>
+  );
 }
-export default AuthedPage
+export default AuthedPage;
--- pages/ctf.js
+++ pages/ctf.js
@@ -1,48 +1,124 @@
-import React from 'react'
+import React from "react";
 
-import Link from 'next/link'
-import { withRouter } from 'next/router'
+import Link from "next/link";
+import { withRouter } from "next/router";
 
-import {CircularProgress} from '@rmwc/circular-progress'
+import { CircularProgress } from "@rmwc/circular-progress";
+import { TextField } from "@rmwc/textfield";
+import { Select } from "@rmwc/select";
 
 import AuthedPage from "../components/AuthedPage";
-import Challenge from "../components/Challenge"
+import Challenge from "../components/Challenge";
 
-import '@rmwc/circular-progress/circular-progress.css';
+import "@rmwc/circular-progress/circular-progress.css";
 
-import css from "./styles/ctf.scss"
+import "@material/textfield/dist/mdc.textfield.css";
+import "@material/floating-label/dist/mdc.floating-label.css";
+import "@material/notched-outline/dist/mdc.notched-outline.css";
+import "@material/line-ripple/dist/mdc.line-ripple.css";
+
+import "@material/select/dist/mdc.select.css";
+import "@material/floating-label/dist/mdc.floating-label.css";
+import "@material/notched-outline/dist/mdc.notched-outline.css";
+import "@material/line-ripple/dist/mdc.line-ripple.css";
+import "@material/list/dist/mdc.list.css";
+import "@material/menu/dist/mdc.menu.css";
+import "@material/menu-surface/dist/mdc.menu-surface.css";
+
+import css from "./styles/ctf.scss";
 import firebase from "firebase";
 
 function CTF(props) {
-  const [db] = React.useState(firebase.firestore())
-  const [dataFetched, setDataFetched] = React.useState([])
-  const [chals, setChals] = React.useState([])
+  const [db] = React.useState(firebase.firestore());
+  const [dataFetched, setDataFetched] = React.useState([]);
+  const [chals, setChals] = React.useState([]);
+  const [categories, setCategories] = React.useState([]);
+  const [searchVal, setSearchVal] = React.useState("");
+  const [categoryVal, setCategoryVal] = React.useState("");
+  const [searchChal, setSearchChal] = React.useState([]);
 
+  // grab data
   React.useEffect(() => {
-    db.collection('ctfs').doc(props.router.query.id).onSnapshot(snapshot => {
-      setDataFetched(snapshot.get('dataFetched'))
-    })
+    db.collection("ctfs")
+      .doc(props.router.query.id)
+      .onSnapshot(snapshot => {
+        setDataFetched(snapshot.get("dataFetched"));
+      });
 
-    db.collection('ctfs').doc(props.router.query.id).collection('chals').onSnapshot(snapshot => {
-      let res = []
+    db.collection("ctfs")
+      .doc(props.router.query.id)
+      .collection("chals")
+      .onSnapshot(snapshot => {
+        let res = [];
+        let categoriesTemp = [""];
+        snapshot.forEach(doc => {
+          res.push(doc.data());
+          if (categoriesTemp.indexOf(doc.data().category) == -1) {
+            categoriesTemp.push(doc.data().category);
+          }
+        });
+        setCategories(categoriesTemp);
+        setSearchChal(res);
+        setChals(res);
+      });
+  }, [props.router.query.id]);
 
-      snapshot.forEach(doc => {
-        res.push(doc.data())
-      })
+  // update based on user search
+  React.useEffect(() => {
+    if (dataFetched) {
+      // temp vars
+      let searchChalTemp = [];
+      let searchValTemp = searchVal.replace(/\s+/g, "").toLowerCase();
+      searchChalTemp = chals.filter((chal, i) => {
+        let nameTemp = chal.name.replace(/\s+/g, "").toLowerCase();
 
-      setChals(res)
-    })
-  }, [props.router.query.id])
+        if (
+          (searchVal == "" || nameTemp.indexOf(searchValTemp) > -1) &&
+          (categoryVal == "" || categoryVal == chal.category)
+        ) {
+          return true;
+        }
+        return false;
+      });
+      console.log(searchChalTemp);
+      setSearchChal(searchChalTemp);
+    }
+  }, [searchVal, categoryVal, dataFetched, chals, categories]);
 
-  return <>
-    <AuthedPage>
-      {dataFetched ?
-        <div className={css.chals}>
-          {chals.map((chal, i) => <Challenge chal={chal} key={i}/>)}
+  return (
+    <>
+      <AuthedPage>
+        <div className={css.searchWrap}>
+          <TextField
+            label="Search"
+            outlined
+            value={searchVal}
+            onChange={e => {
+              setSearchVal(e.target.value);
+            }}
+          />
+          <Select
+            label="Category"
+            outlined
+            options={categories}
+            value={categoryVal}
+            onChange={e => {
+              setCategoryVal(e.target.value);
+            }}
+          />
         </div>
-      : <CircularProgress/>}
-    </AuthedPage>
-  </>
+        {dataFetched ? (
+          <div className={css.chals}>
+            {searchChal.map((chal, i) => (
+              <Challenge chal={chal} key={i} />
+            ))}
+          </div>
+        ) : (
+          <CircularProgress />
+        )}
+      </AuthedPage>
+    </>
+  );
 }
 
 export default withRouter(CTF);
