bc6bda450d02b6bf9ab37eec63736194980e830a
Aidan Goettsch <yayes.aidan@gmail.com>
2019-06-24 16:10:56 -0400

    work on sidebar

--- components/AuthedPage.js
+++ components/AuthedPage.js
@@ -2,17 +2,23 @@ import React from 'react'
 
 import Head from 'next/head'
 import Link from 'next/link'
+import { withRouter } from 'next/router'
 import {Drawer, DrawerHeader, DrawerTitle, DrawerContent} from '@rmwc/drawer'
 import {List, ListItem} from '@rmwc/list'
 import {Button} from '@rmwc/button'
 import {IconButton} from '@rmwc/icon-button'
 import {CircularProgress} from '@rmwc/circular-progress'
+import {TextField} from '@rmwc/textfield'
 
 import '@material/drawer/dist/mdc.drawer.css'
 import '@material/list/dist/mdc.list.css'
 import '@material/button/dist/mdc.button.css';
 import '@material/icon-button/dist/mdc.icon-button.css';
 import '@rmwc/circular-progress/circular-progress.css';
+import '@material/textfield/dist/mdc.textfield.css';
+import '@material/floating-label/dist/mdc.floating-label.css';
+import '@material/notched-outline/dist/mdc.notched-outline.css';
+import '@material/line-ripple/dist/mdc.line-ripple.css';
 
 import css from "./styles/authedPage.scss"
 import MenuIcon from "./icons/material-icons/menu.svg";
@@ -20,11 +26,32 @@ import AddIcon from "./icons/material-icons/add.svg";
 
 import firebase from 'firebase'
 import "firebase/firestore"
+import {Dialog, DialogActions, DialogButton, DialogContent} from "@rmwc/dialog";
+
+// const add = () => {
+//     db.collection("ctfs").add({
+//         name: "HSCTF 2019",
+//         url: "https://ctf.hsctf.com",
+//         dataFetched: false
+//     }).then(docRef => {
+//         console.log(docRef.id)
+//     }).catch(e => {
+//         console.log(e)
+//     })
+// }
 
 function AuthedPage(props) {
   const [drawerOpen, setDrawerOpen] = React.useState(false);
   const [db] = React.useState(firebase.firestore())
   const [ctfs, setCTFs] = React.useState([])
+  const [path, setPath] = React.useState("")
+  const [queryString, setQueryString] = React.useState({})
+  const [addDialogOpen, setAddDialogOpen] = React.useState(false)
+
+  React.useEffect(() => {
+      setPath(props.router.pathname)
+      setQueryString(props.router.query)
+  }, [props.router.pathname, props.router.query])
 
   React.useEffect(() => {
     db.collection('ctfs').onSnapshot(snapshot => {
@@ -41,32 +68,42 @@ function AuthedPage(props) {
     })
   }, [])
 
-  const ctfList = ctfs.map((c, i) => <Link as={`/ctf/${c.id}`} as={`/ctf/${c.id}`} href={`/ctf?id=${c.id}`}>
-    <a>
-      <ListItem key={i} className={css.ctfListItem}>
-        <p>{c.name}</p>
-        {c.dataFetched ? <></> : <CircularProgress/>}
-      </ListItem>
-    </a>
-  </Link>)
+  const ctfList = ctfs.map((c, i) => {
+    console.log(queryString)
+    console.log(path)
+
+    return <Link as={`/ctf/${c.id}`} as={`/ctf/${c.id}`} href={`/ctf?id=${c.id}`}>
+      <a>
+        <ListItem key={i} className={css.ctfListItem} activated={queryString && queryString.id === c.id && path === "/ctf"}>
+          <p>{c.name}</p>
+          {c.dataFetched ? <></> : <CircularProgress/>}
+        </ListItem>
+      </a>
+    </Link>
+  })
 
   return (<>
+      <Dialog
+          open={addDialogOpen}
+          onClose={() => {
+              setAddDialogOpen(false);
+          }}
+      >
+          <DialogContent theme={["onSurface"]}>
+            urgay
+          </DialogContent>
+          <DialogActions>
+            <DialogButton action="close">Download All</DialogButton>
+            <DialogButton action="accept" isDefaultAction>Done</DialogButton>
+          </DialogActions>
+      </Dialog>
+
     <Drawer modal open={drawerOpen} onClose={() => setDrawerOpen(false)} theme={["surface"]}>
       <DrawerHeader>
         <DrawerTitle>CTFSync</DrawerTitle>
       </DrawerHeader>
       <DrawerContent>
-        <Button label={"Add CTF"} icon={<AddIcon/>} onClick={() => {
-          db.collection("ctfs").add({
-            name: "HSCTF 2019",
-            url: "https://ctf.hsctf.com",
-            dataFetched: false
-          }).then(docRef => {
-            console.log(docRef.id)
-          }).catch(e => {
-            console.log(e)
-          })
-        }}/>
+        <Button label={"Add CTF"} icon={<AddIcon/>} onClick={() => setAddDialogOpen(true)}/>
         <List>
           {ctfList}
         </List>
@@ -88,4 +125,4 @@ function AuthedPage(props) {
     </div>
   </>)
 }
-export default AuthedPage
+export default withRouter(AuthedPage)
